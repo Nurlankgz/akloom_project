5bd43d80b12b70c4247c2009140749fb1fa45879
Nurlan <nurikkg060@gmail.com>
2025-04-28 21:04:02 +0600

    maps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import Footer from "../components/footer/Footer";
 import "./globals.css";
 import Home from "./page";
+import Map from "../map";
 
 export default function RootLayout({}) {
   return (
     <html lang="en">
       <body>
-        
         <Home />
+        <Map />
         <Footer />
       </body>
     </html>
